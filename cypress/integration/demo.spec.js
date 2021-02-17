4a2a47cd9eb275e1acccaf5fd841460c7b125f76
Tom McGuire <Mcgtom10@gmail.com>
2021-02-16 17:03:05 -0800

    Update demo.spec.js

--- cypress/integration/demo.spec.js
+++ cypress/integration/demo.spec.js
@@ -1,8 +1,27 @@
-describe("demo", () => {
-  it("should handle aborted requests", () => {
-    cy.intercept("https://jsonplaceholder.typicode.com/todos/1").as("xhr");
-    cy.visit("http://localhost:3000");
-    cy.get("button").contains("bad").click();
-    cy.wait("@xhr");
-  });
-});
+it('should handle non-aborted requests', () => {
+  cy.intercept('https://jsonplaceholder.typicode.com/todos/1').as('xhr')
+  cy.visit('index.html')
+  cy.get('button').contains('good').click()
+  cy.get('pre').contains('delectus') // response body renders to page
+  // ✅ passes
+  cy.wait('@xhr')
+})
+
+it('cy.route should handle aborted requests', () => {
+  cy.server()
+  cy.route('https://jsonplaceholder.typicode.com/todos/1').as('xhr')
+  cy.visit('index.html')
+  cy.get('button').contains('bad').click()
+  cy.get('pre').contains('delectus') // response body renders to page
+  // ✅ passes
+  cy.wait('@xhr')
+})
+
+it('cy.intercept should handle aborted requests', () => {
+  cy.intercept('https://jsonplaceholder.typicode.com/todos/1').as('xhr')
+  cy.visit('index.html')
+  cy.get('button').contains('bad').click()
+  cy.get('pre').contains('delectus')  // response body renders to page
+  // ❗️ times out sometimes
+  cy.wait('@xhr')
+})
